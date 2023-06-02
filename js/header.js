f77aef97d0ed8105f0e7ee3fed84197df74e0599
Momin Rather <momin.rather@gmail.com>
2023-06-02 11:36:31 +0530

    base url testing

--- js/header.js
+++ js/header.js
@@ -1,12 +1,12 @@
-console.log(window.location.href);
+if (!localStorage.getItem("base_url"))
+  localStorage.setItem("base_url", window.location.href);
 
-// if (!localStorage.getItem("base_url"))
-//   localStorage.setItem("base_url", window.location.origin);
+let base_url = localStorage.getItem("base_url");
 
 $("#header").append(`
       <nav class="navbar">
         <div class="container-fluid">
-          <img id="logo" src="../images/logo.png" />
+          <img id="logo" src="${base_url}/images/logo.png" />
           <div class="justify-content-end" id="mynavbar">
             <form class="d-flex">
               <button class="btn" id="signIn" type="button">Sign In</button>
