4444af7cf268f7e832f8e1734bdbc613b980ce1d
Momin Rather <momin.rather@gmail.com>
2023-06-02 12:52:27 +0530

    base url testing

--- js/links.js
+++ js/links.js
@@ -1,7 +1,6 @@
 $("head").append(`
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <base href="${base_url}" target="_self">
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
       rel="stylesheet"
