a87e7aef6d27521a23fda6491822e5e75476c2c4
Momin Rather <momin.rather@gmail.com>
2023-06-02 10:16:27 +0530

    Sign in page

---
+++ js/signIn/signin.js
@@ -0,0 +1,22 @@
+let users = JSON.parse(localStorage.getItem("users"));
+
+$("#signIn").hide();
+
+$("#signup").on("click", () => {
+  window.location.href = "/";
+});
+
+$("#signInPage").on("click", () => {
+  let email = $("#email").val();
+  let password = $("#password").val();
+  let valid = false;
+  for (user of users) {
+    if (user["email"] == email && user["pass"] == password) {
+      valid = true;
+      break;
+    }
+  }
+
+  if (valid) console.log("Signed In Succesfully.");
+  else console.log("Ivalid email or password.");
+});
