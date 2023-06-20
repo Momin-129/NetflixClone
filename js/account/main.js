ecb4e5a83394be9430f7be37f7b537375742397a
Momin-129 <momin.rather@gmail.com>
2023-06-20 10:10:24 +0530

    account page completed

---
+++ js/account/main.js
@@ -0,0 +1,22 @@
+import { Header } from "../headerMain.js";
+import { Footer } from "../footerMain.js";
+import { Links } from "../links.js";
+
+Header();
+Footer();
+Links();
+
+let users = JSON.parse(localStorage.getItem("users"));
+let user_id = sessionStorage.getItem("user_id");
+
+let email = users[user_id]["email"];
+let pass = users[user_id]["pass"];
+let payment = users[user_id]["payMethod"];
+let plan = users[user_id]["plan"];
+
+$("#email").html(email);
+$("#pass").html(pass);
+$("#pass").css("-webkit-text-security", "square");
+$("#payment").html(payment.toUpperCase());
+$("#plan").html(plan.toUpperCase());
+$("#plan").css({ "font-size": "16px", "font-weight": "bold" });
